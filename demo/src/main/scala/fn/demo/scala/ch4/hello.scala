3623f5f31b246bf1ef0c2c5ead383496db3c15d0
ypyao77 <30677472@qq.com>
2018-05-04 20:35:24 +0800

    scala programing demo chapter 5, 6, 7, 8

--- demo/src/main/scala/fn/demo/scala/ch4/hello.scala
+++ demo/src/main/scala/fn/demo/scala/ch4/hello.scala
@@ -2,4 +2,6 @@ package fn.demo.scala.ch4
 
 object Hello extends App {
   println("hello world!")
+  println("""|Welcome to Ultamix 3000.
+             |Type "HELP" for help.""".stripMargin)
 }
---
+++ demo/src/main/scala/fn/demo/scala/ch7/Grep.scala
@@ -0,0 +1,20 @@
+package fn.demo.scala.ch7
+
+object Hello extends App {
+  def fileLines(file: java.io.File) =
+    scala.io.Source.fromFile(file).getLines.toList
+
+  def grep(pattern: String) = {
+    val filesHere = (new java.io.File(".")).listFiles
+    println("filesHere = " + filesHere)
+
+    for {
+      file <- filesHere
+      if file.getName.endsWith(".scala")
+      line <- fileLines(file)
+      if line.trim.matches(pattern)
+    } println(file + ": " + line.trim)
+  }
+
+  grep(".*gcd.*")
+}
