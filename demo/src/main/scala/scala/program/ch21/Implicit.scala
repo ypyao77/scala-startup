241089ebc2e6dfd6c8f8286c167d73cb4e4b97e0
ypyao77 <30677472@qq.com>
2018-05-10 13:55:08 +0800

    programing in scala : demo

---
+++ demo/src/main/scala/scala/program/ch21/Implicit.scala
@@ -0,0 +1,10 @@
+package scala.program.ch21
+
+object Implicit {
+  def main(args: Array[String]): Unit = {
+    implicit def intToString(x: Int) = x.toString
+
+    println("intToString(5): " + intToString(5))
+    println("intToString('a'): " + intToString('a'))
+  }
+}
---
+++ demo/src/main/scala/scala/program/ch23/Person.scala
@@ -0,0 +1,38 @@
+package scala.program.ch23
+
+object Person {
+
+  object Sex extends Enumeration {
+    type Sex = Value
+    val Male, Female = Value
+  }
+
+  case class PersonX(name: String, sex: Sex.Value, children: PersonX*) {
+    override def toString: String = "Person('" + name + "', '" + sex + "'): \n" + children.mkString("\t", "\t", "")
+  }
+
+  def main(args: Array[String]): Unit = {
+    println("Male: " + Sex.Male)
+    val lara = PersonX("Lara", Sex.Female)
+    val bob = PersonX("Bob", Sex.Male)
+    val julie = PersonX("Julie", Sex.Female, lara, bob)
+    val charlie = PersonX("Charlie", Sex.Male, lara, bob)
+    val persons = List(lara, bob, julie, charlie)
+
+    println("lara = " + lara)
+    println("bob = " + bob)
+    println("julie = " + julie)
+    println("charlie = " + charlie)
+    println("persons = " + persons)
+
+    val relat1 = for (p <- persons; if p.sex == Sex.Female; c <- p.children) yield (p.name, c.name)
+    val relat2 = persons.filter(p => p.sex == Sex.Female).flatMap(p => (p.children.map(c => (p.name, c.name))))
+    val relat3 = for (p <- persons; c <- p.children) yield (p.name, c.name)
+    println("relat1 = " + relat1)
+    println("relat2 = " + relat2)
+    println("relat3 = " + relat3)
+
+    val s1 = for (p <- persons; n = p.name; if (n.startsWith("C"))) yield n
+    println("s1 = " + s1)
+  }
+}
