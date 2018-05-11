1b071cfe1a0f7fdd3f873de4f293b62459ce8c44
ypyao77 <30677472@qq.com>
2018-05-11 21:32:31 +0800

    akka start: Demo

---
+++ akka/start/src/main/scala/scala/org/algorithmdog/akkalearning/localApp.scala
@@ -0,0 +1,19 @@
+package org.algorithmdog.akkalearning
+
+import akka.actor.{ActorSystem, Props}
+
+/**
+  * Created by lietal on 2017/1/16.
+  */
+object localApp {
+  def main(args: Array[String]): Unit = {
+    val actorSystem = ActorSystem("SummerSchool")
+    val teacher = actorSystem.actorOf(Props[TeacherActor], "teacher")
+    val student = actorSystem.actorOf(Props(new StudentActor(teacher)), "student")
+
+    student ! 7.toLong; //早上 7 点哦
+    Thread.sleep(1000);
+
+    actorSystem.terminate()
+  }
+}
