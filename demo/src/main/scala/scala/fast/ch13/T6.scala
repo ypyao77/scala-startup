5f7a51bedf547b832d4a38d36edfe3b3f058c080
ypyao77 <30677472@qq.com>
2018-05-21 21:27:51 +0800

    fast learning scala: demo

---
+++ demo/src/main/scala/scala/fast/ch13/T6.scala
@@ -0,0 +1,11 @@
+package scala.fast.ch13
+
+object T6 {
+
+  def main(args: Array[String]): Unit = {
+    val list = List(1, 2, 3, 4, 5)
+
+    println((list :\ List[Int]()) (_ :: _))
+    println((List[Int]() /: list) ((a, b) => b :: a))
+  }
+}
---
+++ demo/src/main/scala/scala/fast/ch13/T8.scala
@@ -0,0 +1,15 @@
+package scala.fast.ch13
+
+object T8 {
+  def div(array: Array[Double], columns: Int) = {
+    array.grouped(columns).toArray
+  }
+
+  def main(args: Array[String]): Unit = {
+    val array = Array(1.0, 2, 3, 4, 5, 6)
+
+    div(array, 3).foreach(line => println(line.mkString(", ")))
+    println()
+    div(array, 4).foreach(line => println(line.mkString(", ")))
+  }
+}
