54b6ee041b523458505aa85d6f8c2813424d4ff2
ypyao77 <30677472@qq.com>
2018-05-26 08:06:20 +0800

    fast learning scala: demo

---
+++ demo/src/main/scala/scala/fast/ch17/T2.scala
@@ -0,0 +1,30 @@
+package scala.fast.ch17
+
+object T2 {
+
+  class Pair[T](var p: (T, T)) {
+    def swap() = {
+      p = (p._2, p._1)
+    }
+
+    override def toString = "Pair(" + p._1.toString + ", " + p._2.toString + ")"
+  }
+
+  object Pair {
+    def apply[T](p: (T, T)) = new Pair(p)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val pair1 = Pair((1, 3))
+    val pair2 = Pair(("aaa", "bbb"))
+
+    println("pair1: " + pair1)
+    println("pair2: " + pair2)
+
+    pair1.swap()
+    pair2.swap()
+
+    println("pair1: " + pair1)
+    println("pair2: " + pair2)
+  }
+}
---
+++ demo/src/main/scala/scala/fast/ch17/T6.scala
@@ -0,0 +1,19 @@
+package scala.fast.ch17
+
+object T6 {
+  def midlle[T](it: Iterable[T]): T = {
+    val list = it.toList
+    list(list.length / 2)
+  }
+
+  def main(args: Array[String]): Unit = {
+    // println(midlle("")) //error
+    // println(midlle(List())) //error
+
+    println(midlle("a"))
+    println(midlle(List(1)))
+
+    println(midlle("middle"))
+    println(midlle(List(1, 2, 3, 4, 5, 6)))
+  }
+}
